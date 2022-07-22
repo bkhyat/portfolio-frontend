420d03679ceca9696e0406d2bcf96ab812ccaa2d
Bkhyat <sbkhyat@gmail.com>
2022-07-23 00:35:52 +0545

    Create todos modal and implement API calls for updating/bulk creating todos

---
+++ frontend/src/component/todo/TodoModal.js
@@ -0,0 +1,58 @@
+import {message, Modal} from "antd";
+import {useForm} from "antd/es/form/Form";
+import {useState} from "react";
+import axios from "axios";
+import ToDoForm from "./TodoForm";
+
+
+const TodoModal = ({isVisible, toggleVisible, todo = []}) => {
+    const [form] = useForm()
+    const [pushingData, setPushingData] = useState(false);
+    const onFinish = (values) => {
+        setPushingData(true)
+        const todosURL = process.env.REACT_APP_API_BASE_URL + '/todo/v1/todos/'
+        if (todo.length === 1) {
+            const {title, description, is_completed} = values.todos[0]
+            axios.patch(todosURL + `${todo[0].id}/`, {title, description, is_completed})
+                .then(
+                    resp => {
+                        console.log(resp.data)
+                        message.success('Todo updated successfully', 3)
+                    }
+                )
+                .catch(error => {
+                    message.error("Error updating todo, try again!", 3)
+                })
+                .finally(() => setPushingData(false))
+        } else {
+            axios.post(todosURL, values.todos)
+                .then(resp => {
+                    console.log(resp.data)
+                    message.success('New todo created successfully!', 3)
+                })
+                .catch(
+                    err => {
+                        console.log(err)
+                        message("An error occurred while creating new todo, try again!", 3)
+                    }
+                )
+                .finally(() => setPushingData(false))
+        }
+    }
+
+    return (
+        <Modal
+            closable
+            title={todo.length === 1 ? 'Update Todo' : 'Create Todos'}
+            visible={isVisible}
+            onCancel={toggleVisible}
+            onOk={form.submit}
+            okButtonProps={{loading: pushingData}}
+            okText={todo.length === 1 ? 'Update' : 'Create'}
+            bodyStyle={{height: '500px', overflowY: 'auto'}}
+        >
+            <ToDoForm form={form} onFinish={onFinish} todo={todo}/>
+        </Modal>
+    )
+}
+export default TodoModal;

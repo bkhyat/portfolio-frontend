27990653978f11d5825545110413f659130656c0
Bkhyat <sbkhyat@gmail.com>
2022-10-15 20:51:39 +0545

    Remove unnecessary console log

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -3,7 +3,6 @@ import axios from "axios";
 const instance = axios.create();
 
 instance.interceptors.request.use(function (config) {
-    console.log('Interceptor')
     const access = localStorage.getItem('access')
     if (access) {
         return {...config, headers: {...config.headers, Authorization: access}}

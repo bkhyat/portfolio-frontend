b7e52f25fee4116ee5901739a0e213288ed4d180
Bkhyat <sbkhyat@gmail.com>
2022-10-15 20:52:08 +0545

    Use New Log and Log of the day components in Time Logger

--- frontend/src/component/timeLogger/index.js
+++ frontend/src/component/timeLogger/index.js
@@ -1,8 +1,29 @@
-const TimeLogger = () => {
+import {useEffect} from "react";
+import {useDispatch} from "react-redux";
+import {fetchLogs, toggleLoading} from "../../rtk/timeLogger/slices";
+import {Card, Divider, Row,} from "antd";
+import NewTimeLogger from "./newTimeLogger";
+import LogOfTheDay from "./logOfTheDay";
 
-    return <div>
-        Time Logger
-    </div>
+const TimeLogger = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(toggleLoading())
+        dispatch(fetchLogs())
+
+    }, [])
+
+    return <Row>
+        <Card style={{width: '30%'}}>
+            <Divider>Enter New Log</Divider>
+            <NewTimeLogger/>
+        </Card>
+        <Card style={{width: '70%'}}>
+            <Divider>Today's Logs</Divider>
+            <LogOfTheDay/>
+        </Card>
+    </Row>
 }
 
 export default TimeLogger;

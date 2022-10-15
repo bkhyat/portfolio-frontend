e052498b9eba47398dd9cd211a6abb57efbeebaa
Bkhyat <sbkhyat@gmail.com>
2022-10-15 20:42:38 +0545

    Create services and reducers to enter a new time log

--- frontend/src/rtk/timeLogger/services.js
+++ frontend/src/rtk/timeLogger/services.js
@@ -2,10 +2,13 @@ import axios from '../../axiosConfig';
 
 const BASE_URL = process.env.REACT_APP_API_BASE_URL + "/time-logger/v1"
 
-const fetchLogs = (date, search) => axios.get(BASE_URL)
+const fetchLogs = (date, search) => axios.get(BASE_URL + '/')
+
+const pushLog = (data) => axios.post(BASE_URL + '/', data)
 
 const timeLoggerService = {
-    fetchLogs
+    fetchLogs,
+    pushLog
 }
 
 export default timeLoggerService;
--- frontend/src/rtk/timeLogger/slices.js
+++ frontend/src/rtk/timeLogger/slices.js
@@ -14,12 +14,35 @@ export const fetchLogs = createAsyncThunk(
                 message: "Error fetching logs",
                 description: e?.response?.data?.detail || e?.message || "Error fetching Log, try again later!"
             })
+            return thunkAPI.rejectWithValue()
+        }
+    }
+)
+
+export const enterLog = createAsyncThunk(
+    "timeLogger/enterLog",
+    async (data, thinkAPI) => {
+        try {
+            const resp = await timeLoggerService.pushLog(data)
+            return resp.data
+        } catch (e) {
+            let message;
+            message = Object.keys(e?.response?.data || {}).map(key => (`${key}: ${e.response.data[key]}`)).join("\n")
+            if (!message) {
+                message = "Error Entering Log, Please try again later!"
+            }
+            notification.error({
+                message: "Error Pushing log",
+                description: message
+            })
+            return thinkAPI.rejectWithValue()
         }
     }
 )
 
 const initialState = {
     isLoading: false,
+    isPushing: false,
     logsOfTheDay: [],
     logsByDate: {}
 }
@@ -29,11 +52,21 @@ const timeLoggerSlice = createSlice(
         name: 'timeLogger',
         initialState,
         reducers: {
-            toggleLoading: (state) => ({...state, isLoading: !state.isLoading})
+            toggleLoading: (state) => ({...state, isLoading: !state.isLoading}),
+            togglePushing: (state) => ({...state, isPushing: !state.isPushing})
         },
         extraReducers: {
             [fetchLogs.fulfilled]: (state, action) => {
                 return {...state, isLoading: false, logsOfTheDay: action.payload}
+            },
+            [fetchLogs.rejected]: (state) => {
+                return {...state, isLoading: false}
+            },
+            [enterLog.fulfilled]: (state, action) => {
+                return {...state, logsOfTheDay: [...state.logsOfTheDay, action.payload], isPushing: false}
+            },
+            [enterLog.rejected]: (state) => {
+                return {...state, isPushing: false}
             }
         }
     }

40356dde86574a5057a3efcff10d8494cc4a6db1
Bkhyat <sbkhyat@gmail.com>
2022-10-16 21:52:38 +0545

    Prepare query params for filtered logs based on user inputs

--- frontend/src/component/timeLogger/filterLogs.js
+++ frontend/src/component/timeLogger/filterLogs.js
@@ -1,14 +1,37 @@
 import {Button, DatePicker, Form, Input} from "antd";
 import {SearchOutlined} from "@ant-design/icons";
+import {useForm} from "antd/es/form/Form";
+import {DATE_FORMAT} from "../constants";
+import {useDispatch} from "react-redux";
+import {filterLogs} from "../../rtk/timeLogger/slices";
 
 const FilterLogs = () => {
+    const [form] = useForm();
+    const dispatch = useDispatch();
+
+    const onFinish = values => {
+        let {dates, search} = values
+        const queryParam = {}
+        if (dates) {
+            if (dates[0] === dates[1]) {
+                queryParam['date'] = dates[0].format(DATE_FORMAT)
+            } else {
+                queryParam['start_date'] = dates[0].format(DATE_FORMAT)
+                queryParam['end_date'] = dates[1].format(DATE_FORMAT)
+            }
+        }
+        if (search) {
+            queryParam['search'] = search
+        }
+        dispatch(filterLogs(queryParam))
+    }
 
     return <div>
-        <Form layout={'inline'} size={'small'}>
-            <Form.Item>
-                <DatePicker.RangePicker/>
+        <Form layout={'inline'} size={'small'} form={form} onFinish={onFinish}>
+            <Form.Item name={'dates'}>
+                <DatePicker.RangePicker format={DATE_FORMAT}/>
             </Form.Item>
-            <Form.Item>
+            <Form.Item name={'search'}>
                 <Input placeholder={"Search in description"} style={{width: '400px'}}/>
             </Form.Item>
             <Form.Item>

637c24866c0340ca3bde2bb89da23519c854d1f0
Bkhyat <sbkhyat@gmail.com>
2022-10-16 20:59:11 +0545

    Add form to filter log component

--- frontend/src/component/timeLogger/filterLogs.js
+++ frontend/src/component/timeLogger/filterLogs.js
@@ -1,7 +1,20 @@
+import {Button, DatePicker, Form, Input} from "antd";
+import {SearchOutlined} from "@ant-design/icons";
+
 const FilterLogs = () => {
 
     return <div>
-        Filter Logs
+        <Form layout={'inline'} size={'small'}>
+            <Form.Item>
+                <DatePicker.RangePicker/>
+            </Form.Item>
+            <Form.Item>
+                <Input placeholder={"Search in description"} style={{width: '400px'}}/>
+            </Form.Item>
+            <Form.Item>
+                <Button htmlType={'submit'} icon={<SearchOutlined/>} type={'primary'}>Filter</Button>
+            </Form.Item>
+        </Form>
     </div>
 }
 

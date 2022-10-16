a80056093c3b69d3d8ecaa3511cda0c87c553fe9
Bkhyat <sbkhyat@gmail.com>
2022-10-16 21:52:21 +0545

    update fetchLogs services to support query params for filtered logs

--- frontend/src/rtk/timeLogger/services.js
+++ frontend/src/rtk/timeLogger/services.js
@@ -2,11 +2,13 @@ import axios from '../../axiosConfig';
 
 const BASE_URL = process.env.REACT_APP_API_BASE_URL + "/time-logger/v1"
 
-const fetchLogs = (date, search) => {
-    const params = {}
-    if (!(date || search)) {
+const fetchLogs = (queryParams) => {
+    let params = {}
+    if (!queryParams) {
         // Pass today's date if both date and search are empty
         params['date'] = new Date().toJSON().slice(0, 10)
+    } else {
+        params = queryParams
     }
     return axios.get(BASE_URL + '/', {params})
 }

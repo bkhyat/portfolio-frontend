38a89ffd3f1f92b3db861d0decd1321b14551a42
Bikhyat Adhikari <sbkhyat@gmail.com>
2022-11-05 13:11:23 +0545

    Decrease padding of Profile component to match with the padding in experience bullet points

--- frontend/src/component/resume/Profile.js
+++ frontend/src/component/resume/Profile.js
@@ -1,6 +1,6 @@
 const Profile = ({profiles}) => {
 
-    return <ul style={{textAlign: 'justify', paddingRight: '6px'}}>
+    return <ul style={{textAlign: 'justify', paddingLeft:26, paddingRight: '6px'}}>
         {profiles.map((item, index) => <li key={index}>{item}</li>)}
     </ul>
 }

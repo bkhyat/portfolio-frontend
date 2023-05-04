0b14145813226f3f45be595980a2df931beccd16
Bikhyat Adhikari <sbkhyat@gmail.com>
2023-05-04 22:59:33 +0545

    Vocab component initial set up

--- frontend/src/component/vocab/index.js
+++ frontend/src/component/vocab/index.js
@@ -4,7 +4,7 @@ import WordsOfTheDay from "./WordsOfTheDay";
 const Vocab = () => {
 
     return <div  style={{
-        position: 'absolute', left: '50%', top: '50%',
+        position: 'absolute', left: '50%', top: '10%',
         transform: 'translate(-50%, -50%)'
     }}><WordsOfTheDay/></div>
 }
